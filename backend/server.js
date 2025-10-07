803d27d9b9227d61af66a49010fb1c58cb680c2b
Pedro <pedrominato23@gmail.com>
2025-10-07 09:21:42 -0300

    Criando funcoes de ler e gravar em server.js

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,23 @@ app.use(express.json())
 
 const secret_key = process.env.SECRET_KEY
 
+// Local do arquivo (simula banco de dados)
+const localUsuarios = path.join(__dirname, 'usuarios.js')
+
+// Criando funcao para ler o arquivo usuario
+const consultarUsuario = ()=>{
+    const data = fs.readFileSync(localUsuarios, "utf-8")
+    return JSON.parse(data)
+}
+
 app.listen(PORT, (req,res)=>{
     console.log(`Servidor rodando na porta http://localhost:${PORT}`)
 })
+
+//Funcao para gravar dados no arquivo usuarios
+
+const salvarUsuarios = (users)=>{
+    fs.writeFileSync(localUsuarios,JSON.stringify(users,null,2))
+        
+    
+}
