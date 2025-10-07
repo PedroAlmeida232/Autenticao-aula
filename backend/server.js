7f83007d3048a77428c5310aa1c5a33d44ee0a55
Pedro <pedrominato23@gmail.com>
2025-10-07 09:15:17 -0300

    Criando estrutura inicial do backend (server.js)

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
+// Baixando dependencias
+require('dotenv').config();
 const express = require('express')
-
+const fs = require("fs")
+const path = require("path")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const cors = require('cors')
 const app = express()
 
-PORT = 3000
+const PORT = 5001
+
+app.use(cors())
+app.use(express.json())
+
+const secret_key = process.env.SECRET_KEY
 
 app.listen(PORT, (req,res)=>{
-    console.log(`Servidor rodando na porta ${PORT}`)
+    console.log(`Servidor rodando na porta http://localhost:${PORT}`)
 })
